ba4e4604aa50e5e634c42220f731ac94da0dee6c
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-07-24 15:18:30 +0300

    feat: add section heroes and section footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-$(function(){
-  $('[data-fancybox]').fabcybox({
+$(function () {
+
+
+
+  $('[data-fancybox]').fancybox({
     youtube: {
       controls: 0,
       showinfo: 0
     }
   });
 
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="./images/hero-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="./images/hero-right.png" alt="next"></button>',
+
+  });
+
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false,
+  });
 
 });
+
+console.log('nrn');
