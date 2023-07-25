d5a3aa893269f0556fd61d08bbbc9ccef14da86c
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-07-25 18:54:39 +0300

    feat: add adpdtive for index.html and season-1.html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 $(function () {
+  $('.menu__btn').on('click', function() {
+    $('.menu__list').toggleClass('menu__list--active')
+  })
+
   $('.heroes__slider-img').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button class="slick-btn slick-prev"><img src="./images/hero-left.png" alt="prev"></button>',
     nextArrow: '<button class="slick-btn slick-next"><img src="./images/hero-right.png" alt="next"></button>',
-
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
   });
 
   $('.heroes__slider-text').slick({
